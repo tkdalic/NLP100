1e3158d44e25032492ac9c915b7d43562724510a
Riku Takada <rikutakada@RikunoMacBook-Air.local>
2016-10-23 11:29:15 +0900

    [update]04.pyの回答をlistからmapに変更

--- 1st/04.py
+++ 1st/04.py
@@ -4,14 +4,18 @@
 if __name__ == '__main__':
     str = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
     num = [1, 5, 6, 7, 8, 9, 15, 16, 19, 0]
+    anlis = []
+    ans = {}
     for k in range(0, len(num)):
         num[k] -= 1
-    ans = []
+    anlis = []
     for k, v in enumerate(str.split()):
         if k == num[0]:
             num.pop(0)
-            ans.append(v[0])
+            anlis.append(v[0])
         else:
-            ans.append(v[0:2])
+            anlis.append(v[0:2])
+    for k, v in enumerate(anlis):
+        ans[v] = k + 1
 
     print(ans)
