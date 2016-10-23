7300075da7a40808a0846a1953cbc1451311c5ee
Riku Takada <rikutakada@RikunoMacBook-Air.local>
2016-10-23 09:03:03 +0900

    [add]02.pyの追加

---
+++ 1st/02.py
@@ -0,0 +1,12 @@
+#!/Users/rikutakada/.pyenv/shims python
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    str1 = 'パトカー'
+    str2 = 'タクシー'
+    ans = ''
+
+    for i, v in enumerate(str1):
+        ans += v + str2[i]
+
+    print(ans)
