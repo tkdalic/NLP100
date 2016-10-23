d32cde6dd40b939e24f7cda3f7e03baf9efa2858
Riku Takada <rikutakada@RikunoMacBook-Air.local>
2016-10-23 09:08:34 +0900

    [add]03.pyの追加

---
+++ 1st/03.py
@@ -0,0 +1,10 @@
+#!/Users/rikutakada/.pyenv/shims python
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+    ans = []
+    for v in str.split():
+        ans.append(v[0])
+
+    print(ans)
