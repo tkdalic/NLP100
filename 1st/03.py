5b08d22b12ea9dca26b1f0f5b4916a9e20435697
Riku Takada <rikutakada@RikunoMacBook-Air.local>
2016-10-24 17:17:34 +0900

    [fix]03.pyの課題を修正

--- 1st/03.py
+++ 1st/03.py
@@ -3,8 +3,8 @@
 
 if __name__ == '__main__':
     str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+    str = str.replace(',', '').replace('.', '')
     ans = []
     for v in str.split():
-        ans.append(v[0])
 
-    print(ans)
+        print(len(v), end='')
