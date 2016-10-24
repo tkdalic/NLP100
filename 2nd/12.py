d910df19e13277f34bc3794b0c6f76a11863fe59
Riku Takada <rikutakada@RikunoMacBook-Air.local>
2016-10-24 21:48:28 +0900

    [fix]12.pyの修正
    内容を勘違いしていました。
    正しくなったと思われます。

--- 2nd/12.py
+++ 2nd/12.py
@@ -3,5 +3,11 @@
 
 if __name__ == '__main__':
     hightemp = open('hightemp.txt', 'r', encoding='utf-8')
-    open('col1.txt', 'w', encoding='utf-8').write(hightemp.readline())
-    open('col2.txt', 'w', encoding='utf-8').write(hightemp.readline())
+
+    col1 = open('col1.txt', 'w', encoding='utf-8')
+    col2 = open('col2.txt', 'w', encoding='utf-8')
+
+    for row in hightemp:
+        words = row.split('\t')
+        col1.write(words[0] + '\n')
+        col2.write(words[1] + '\n')
