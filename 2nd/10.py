9990894fd91f16ecb236cc038d6a7159f5628656
Riku Takada <rikutakada@RikunoMacBook-Air.local>
2016-10-24 22:17:29 +0900

    [update]10.pyの関数化
    15.pyのために関数化

--- 2nd/10.py
+++ 2nd/10.py
@@ -1,5 +1,10 @@
 #!/Users/rikutakada/.pyenv/shims python
 # -*- coding: utf-8 -*-
 
+
+def wc(filename):
+    return open(filename, encoding='utf-8').read().count('\n')
+
+
 if __name__ == '__main__':
-    print(open('hightemp.txt', encoding='utf-8').read().count('\n'))
+    print(wc('hightemp.txt'))
