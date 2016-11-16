1ec35dc42588463da692dc7e4d488530bcc41dae
Riku Takada <rikutakada@RikunoMacBook-Air.local>
2016-11-16 22:39:24 +0900

    [changed]apiを叩くように修正

--- nlp3rd/nlp29.py
+++ nlp3rd/nlp29.py
@@ -1,8 +1,11 @@
 #!/Users/rikutakada/.pyenv/shims python
 # -*- coding: utf-8 -*-
 from nlp28 import infobox4
+import requests
+import json
 
 
 if __name__ == '__main__':
     ans = infobox4()
-    print('https://www.mediawiki.org/wiki/File:' + ans['国旗画像'].replace(' ', '_'))
+    print(json.loads(requests.get(
+        'https://ja.wikipedia.org/w/api.php?action=query&prop=imageinfo&format=json&iiprop=url&titles=Image:' + ans['国旗画像']).text)['query']['pages']['-1']['imageinfo'][0]['url'])
